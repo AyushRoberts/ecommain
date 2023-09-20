e62be30f44932c16422737e5c3ae0edbc3688f1f
AyushRoberts <aroberts00740@gmail.com>
2023-09-20 21:16:10 +0530

    prodpage

--- ecommerce/src/pages/ProductDetails.jsx
+++ ecommerce/src/pages/ProductDetails.jsx
@@ -26,7 +26,21 @@ const ProductDetails = () => {
           <div className=" flex flex-1 justify-center items-center mb-8 lg:mb-0">
             <img className="max-w-[200px] lg:max-w-sm" src={image} alt="" />
           </div>
-          <div className=" flex-1 text-center lg:text-left"></div>
+          <div className=" flex-1 text-center lg:text-left">
+            <h1 className="font-medium mb-2 max-w-[450px] mx-auto text-[26px] lg:mx-0">
+              {title}
+            </h1>
+            <div className="text-xl text-red-500 font-medium mb-6">
+              $ {price}
+            </div>
+            <p className="mb-8">{description}</p>
+            <button
+              className="bg-primary py-4 px-8 text-white"
+              onClick={() => addToCart(thisProd, thisProd.id)}
+            >
+              Add To Cart
+            </button>
+          </div>
         </div>
       </div>
     </section>
